d5896534f0ed42697012fc2007808753e3f5ea0d
martin.emmanuel.santos <martin.emmanuel.santos@gmail.com>
2017-01-06 01:13:33 +1100

    Initial commit

---
+++ app/src/main/java/com/martinemmanuelsantos/medbox/MedicationScheduleActivity.java
@@ -0,0 +1,13 @@
+package com.martinemmanuelsantos.medbox;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MedicationScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medication_schedule);
+    }
+}
---
+++ app/src/main/java/com/martinemmanuelsantos/medbox/NextMedicationFragment.java
@@ -0,0 +1,17 @@
+package com.martinemmanuelsantos.medbox;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class NextMedicationFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_next_medication, container, false);
+        return view;
+    }
+}
